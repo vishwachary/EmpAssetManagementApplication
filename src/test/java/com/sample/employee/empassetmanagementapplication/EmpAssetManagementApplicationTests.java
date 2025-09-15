a93c17e4a06119b663ca5ca444dfd99c0865dab1
VishwaVaranasi <vishweswara.varanasi@oracle.com>
2025-09-15 13:33:09 +0530

    first commit

---
+++ src/test/java/com/sample/employee/empassetmanagementapplication/EmpAssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.employee.empassetmanagementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpAssetManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
